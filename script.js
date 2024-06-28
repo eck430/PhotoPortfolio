bc36b8df55f8f9c792976390a2f9c477d9f433f6
eck430 <estherkim430@gmail.com>
2024-06-28 13:37:50 -1000

    staging albums in js before receiving imgs

---
+++ script.js
@@ -0,0 +1,14 @@
+function loadAlbum(albumName){
+    document.getElementById('mainContent').innerHtml = '';   //clear everything in main
+
+    const addContent = document.getElementById('mainContent').innerHTML;  // adding to <main>
+/*
+    if(albumName = "album1Name"){
+        addContent = `
+            <div class="imgs">
+                <img src=" " alt" ">
+            </div>
+        `;
+    }
+*/
+}
