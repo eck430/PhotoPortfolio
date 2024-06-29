e350290ccea464f5944720bd9e76bdd2efb7d2d3
eck430 <estherkim430@gmail.com>
2024-06-28 14:43:36 -1000

    idea 1 didnt work trying new, adding albums file

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function loadAlbum(albumName){
-    document.getElementById('mainContent').innerHtml = '';   //clear everything in main
+function loadAlbum(contentName){
 
-    const addContent = document.getElementById('mainContent').innerHTML;  // adding to <main>
-/*
-    if(albumName = "album1Name"){
-        addContent = `
-            <div class="imgs">
-                <img src=" " alt" ">
-            </div>
-        `;
-    }
-*/
 }
