79a74268193c93ccda2a7aefadcf99aca2df2868
eck430 <estherkim430@gmail.com>
2024-07-01 16:50:33 -0400

    set main content of album

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 // dynamically load images based on selected album
 
 document.addEventListener('DOMContentLoaded', function(){     //event listener wait for doc to be fully loaded and parsed before executing
-    const albValue = new URLSearchParams(window.location.search).get('alb');     // get key alb value from query param
+    const albParam = new URLSearchParams(window.location.search).get('alb');     // get key alb value from query param
 
     if(albParam){
-        loadAlbum(albValue);
+        loadAlbum(alb);
     }
 });
 
 function loadAlbum(alb){
-    // get <main> where imgs will be displayed
-    const albContent = document.getElementById('albContent');    
+    // reference <main> where imgs will be displayed
+    const albContent = document.getElementById('albContent');
 
     // create obj to map albums to images
-    const albums = {    
+    const albums = {
         'album1': [''],
         'album2': [''],
         'album3': ['']
     };
 
-    // check if user selected album exists
-    if(albums){
+    // check if user selected album exists (if objs key = query key 'alb')
+    if(albums.hasOwnProperty(alb)){
+        // get imgs from album (select arr)
+        const images = albums[alb];
 
+        // create HTML to display images by iterating through images of selected array
+        let html = '<div class = "imgView">';
+        images.forEach(image => {
+            html += `<img src="${image}" alt="album image">`;
+        });
+
+        html += '</div>';
+
+        // set created HTML
+        albContent.innerHTML = html;
+    }else{
+        albContent.innerHTML = '<p>Images not found.</p>'
     }
-
-    // get imgs from album (select arr)
-
-    // create HTML to display images by iterating through images of selected array
-
-    // add created HTML to <main>
 }
